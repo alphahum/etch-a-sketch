958093fde8ff65ebc4c962ce292275ce50ce3c44
alphahum <216708197+alphahum@users.noreply.github.com>
2025-09-03 21:53:47 +0500

    changed the js and css to better get the user prompt and added container in html

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const grid = document.querySelector(".grid");
-for (let i = 0; i < 16 * 16; i++) {
-    const cell = document.createElement("div");
-    grid.appendChild(cell);
+const newgrid = document.querySelector("#size");
+
+function createGrid(size) {
+    grid.innerHTML = "";
+    for (let i = 0; i < size * size; i++ ) {
+        const cell = document.createElement("div");
+        cell.style.flex = `1 0 calc(100% / ${size})`;
+        cell.style.height = `calc(100% / ${size})`;
+        cell.style.boxSizing = "border-box";
+        cell.style.border = "1px solid #ddd";
+
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "black"
+        });
+        grid.appendChild(cell);
+    }
 }
+
+createGrid(16)
